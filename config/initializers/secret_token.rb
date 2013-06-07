274d05cfdb8842b66f004aa28f33ebad08259c1d
André Drougge <andre@redbet.com>
2013-06-07 14:21:25 +0200

    Add Rails project excluding app/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cer::Application.config.secret_key_base = '70f1bf04e9c2a0d699ef9ae91d216229b5435316e6531cd828696d316e7eeb700f3593fc324fa974dfb1ab4a65e86a2abe8dc3c147cf1126dcd1cea3164a66e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cer::Application.config.session_store :cookie_store, key: '_cer_session'
