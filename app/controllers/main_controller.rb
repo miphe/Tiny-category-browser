eebb297e973d0d4ba890fa30357d4d05679deb9e
André Drougge <andre@redbet.com>
2013-06-07 21:04:18 +0200

    Add possibility to navigate another level (1,2)

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,20 +1,28 @@
 class MainController < ApplicationController
 
 	def index
-		# Get data from lib.
-		@cats = [
+		@cats = all_categories
+	end
+
+	def category
+		not_found unless params[:id] && !get_category_by_id(params[:id]).empty?
+		@category = get_category_by_id(params[:id])
+	end
+
+	def all_categories
+		[
 			{
 				:id => 'core_english',
-				:name => 'Core Enlish',
+				:name => 'Core English',
 				:description => 'A course in core English which will teach you all about the core parts about the English language.',
 				:catch_phrase => 'Without a core, everything implodes!',
 				:translation_language => "ja",
-				:icon_image => '/images/category/icon.png',
-				:main_image => '/images/category/main.png',
-				:background_image => '/images/category/background.png',
+				:icon_image => 'category/icon.jpg',
+				:main_image => 'category/main.jpg',
+				:background_image => 'category/background.jpg',
 				:primary_color => "#333",
 				:secondary_color => "#337733",
-				:position => 2,
+				:position => 1,
 				:series => [
 					{
 
@@ -23,13 +31,13 @@ class MainController < ApplicationController
 			},
 			{
 				:id => 'intermediate_english',
-				:name => 'Intermediate Enlish',
+				:name => 'Intermediate English',
 				:description => 'A course in Intermediate English which will teach you all about the Intermediate parts about the English language.',
 				:catch_phrase => 'Without a Intermediate, there is not much to speak about!',
 				:translation_language => "ja",
-				:icon_image => '/images/category/icon.png',
-				:main_image => '/images/category/main.png',
-				:background_image => '/images/category/background.png',
+				:icon_image => 'category/icon.jpg',
+				:main_image => '/category/main.jpg',
+				:background_image => '/category/background.jpg',
 				:primary_color => "#333",
 				:secondary_color => "#337733",
 				:position => 2,
@@ -41,23 +49,36 @@ class MainController < ApplicationController
 			},
 			{
 				:id => 'advanced_english',
-				:name => 'Advanced Enlish',
+				:name => 'Advanced English',
 				:description => 'A course in Advanced English which will teach you all about the Advanced parts about the English language.',
 				:catch_phrase => 'Without a Advanced, you wont impress anyone!',
 				:translation_language => "ja",
-				:icon_image => '/images/category/icon.png',
-				:main_image => '/images/category/main.png',
-				:background_image => '/images/category/background.png',
+				:icon_image => 'category/icon.jpg',
+				:main_image => 'category/main.jpg',
+				:background_image => 'category/background.jpg',
 				:primary_color => "#333",
 				:secondary_color => "#337733",
-				:position => 2,
+				:position => 3,
 				:series => [
 					{
 
 					}
 				]
 			}
-		]
+		].sort_by { |c| c[:position] }
 	end
 
+	def get_category_by_id (id)
+		result = {}
+
+		all_categories.each do |c|
+			result = c[:id] == id ? c : result
+		end
+
+		result
+	end
+
+	def not_found
+	  raise ActionController::RoutingError.new('Not Found')
+	end
 end
